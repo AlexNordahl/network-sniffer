78d91bdff7f5c58bb79feba9553297707c6ea859
AlexNordahl <nordahlalex@gmail.com>
2025-12-04 19:12:49 +0100

    formatted file

--- src/helpers/protocol_keyword.h
+++ src/helpers/protocol_keyword.h
@@ -3,25 +3,34 @@
 
 namespace protocol
 {
-
-std::string toKeyword(const int decimal)
-{
-    switch (decimal)
+    std::string toKeyword(const int decimal)
     {
-    case 1: return "ICMP";
-    case 2: return "IGMP";
-    case 4: return "IPV4";
-    case 6: return "TCP";
-    case 17: return "UDP";
-    case 41: return "IPV6";
-    case 47: return "GRE";
-    case 50: return "ESP";
-    case 51: return "AH";
-    case 89: return "OSPF";
-    default: return "?";
+        switch (decimal)
+        {
+        case 1:
+            return "ICMP";
+        case 2:
+            return "IGMP";
+        case 4:
+            return "IPV4";
+        case 6:
+            return "TCP";
+        case 17:
+            return "UDP";
+        case 41:
+            return "IPV6";
+        case 47:
+            return "GRE";
+        case 50:
+            return "ESP";
+        case 51:
+            return "AH";
+        case 89:
+            return "OSPF";
+        default:
+            return "?";
+        }
     }
-}
-
 }
 
 #endif
