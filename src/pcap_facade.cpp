5de0a1d57f964b68306ebc2a92ff870102044199
AlexNordahl <nordahlalex@gmail.com>
2025-12-04 21:13:46 +0100

    fixed size of payload to 20

--- src/pcap_facade.cpp
+++ src/pcap_facade.cpp
@@ -139,7 +139,7 @@ std::pair<EtherFrame, const u_char*> PcapFacade::next()
 std::pair<IpHeader, const u_char*> PcapFacade::parseIPV4(const u_char* payload)
 {
     IpHeader ip{};
-    std::memcpy(&ip, payload, sizeof(IpHeader));
+    std::memcpy(&ip, payload, 20);
 
     uint8_t ihl = ip.ver_ihl & 0x0F;
     size_t headerLength = ihl * 4;
@@ -152,15 +152,7 @@ std::pair<IpHeader, const u_char*> PcapFacade::parseIPV4(const u_char* payload)
 TcpHeader PcapFacade::parseTCP(const u_char* data)
 {
     TcpHeader tcp {};
-    std::memcpy(&tcp, data, sizeof(tcp));
-
-    tcp.srcPort = ntohs(tcp.srcPort);
-    tcp.dstPort = ntohs(tcp.dstPort);
-    tcp.seqNumber = ntohl(tcp.seqNumber);
-    tcp.ackNumber = ntohl(tcp.ackNumber);
-    tcp.windowSize = ntohs(tcp.windowSize);
-    tcp.checksum = ntohs(tcp.checksum);
-    tcp.urg_pointer= ntohs(tcp.urg_pointer);
+    std::memcpy(&tcp, data, 20);
 
     return tcp;
 }
@@ -169,6 +161,7 @@ ArpHeader PcapFacade::parseARP(const u_char* payload)
 {
     ArpHeader arp{};
     memcpy(&arp, payload, sizeof(ArpHeader));
+
     return arp;
 }
 
